8ba5c5142886ecab849c6d83afa1513d63302dc0
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-15 15:32:49 +0700

    implement update dan delete

--- aplikasi-absensi/src/test/java/com/artivisi/absensi/dao/KehadiranDaoTest.java
+++ aplikasi-absensi/src/test/java/com/artivisi/absensi/dao/KehadiranDaoTest.java
@@ -6,6 +6,7 @@ package com.artivisi.absensi.dao;
 
 import com.artivisi.absensi.dao.jdbc.KehadiranDao;
 import com.artivisi.absensi.domain.Kehadiran;
+import java.util.Date;
 import java.util.List;
 import org.junit.Test;
 
@@ -25,5 +26,26 @@ public class KehadiranDaoTest {
             System.out.println("Jam Masuk : "+kehadiran.getJamMasuk());
             System.out.println("Jam Pulang : "+kehadiran.getJamPulang());
         }
+    }
+    
+    @Test
+    public void testInsert() throws Exception {
+        Kehadiran k = new Kehadiran();
+        k.setJamMasuk(new Date());
+        k.setJamPulang(new Date());
+        
+        KehadiranDao kd = new KehadiranDao();
+        kd.simpan(k);
+    }
+    
+    @Test
+    public void testUpdate() throws Exception {
+        Kehadiran k = new Kehadiran();
+        k.setId(1);
+        k.setJamMasuk(new Date());
+        k.setJamPulang(new Date());
+        
+        KehadiranDao kd = new KehadiranDao();
+        kd.simpan(k);
     }
 }
