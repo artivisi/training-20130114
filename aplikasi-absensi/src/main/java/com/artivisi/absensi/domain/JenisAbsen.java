4287346b7af0946ef3d0afa9abac315c101b8814
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-15 13:45:02 +0700

    class Ketidakhadiran dan JenisAbsen

---
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/domain/JenisAbsen.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.absensi.domain;
+
+/**
+ *
+ * @author endy
+ */
+public enum JenisAbsen {
+    SAKIT,IJIN,CUTI,TANPA_KETERANGAN
+}
