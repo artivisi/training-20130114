2d4565061e5a596d3f38741f317b997aaff8626e
bebenpiko <bebenpiko@yahoo.com>
2013-01-15 15:14:42 +0700

    scema tabel Jenis

--- aplikasi-absensi/src/main/sql/skema-postgresql.sql
+++ aplikasi-absensi/src/main/sql/skema-postgresql.sql
@@ -6,3 +6,15 @@ create table kehadiran (
 
 insert into kehadiran (jam_masuk, jam_pulang) 
 values ('2013-01-15 08:00:00', '2013-01-15 16:00:00');
+
+create table jenis (
+    kode integer,
+    nama Varchar(20),
+    denda_telat double precision,
+    tarif_lembur double precision
+    
+      
+);
+
+insert into jenis (kode,nama,denda_telat,tarif_lembur) 
+values ('2','benny',2000,50000);
