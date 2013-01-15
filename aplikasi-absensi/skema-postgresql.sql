d639677f68947fad34dcd301aec1e69a3ed56412
ustadho <cak_ust@yahoo.co.id>
2013-01-15 15:06:14 -0800

    Peserta Test

--- aplikasi-absensi/skema-postgresql.sql
+++ aplikasi-absensi/skema-postgresql.sql
@@ -6,3 +6,10 @@ create table kehadiran (
 
 insert into kehadiran (jam_masuk, jam_pulang) 
 values ('2013-01-15 08:00:00', '2013-01-15 16:00:00');
+
+create table peserta(
+int serial primary key, 
+nama varchar(50)
+);
+
+insert into peserta(nama) values('Ustadho');
