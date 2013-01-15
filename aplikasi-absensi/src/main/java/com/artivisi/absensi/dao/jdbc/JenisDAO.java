34d8478d486fe0b2744620940bb5c10d9c41744d
bebenpiko <bebenpiko@yahoo.com>
2013-01-15 18:13:19 +0700

    spring

--- aplikasi-absensi/src/main/java/com/artivisi/absensi/dao/jdbc/JenisDAO.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/dao/jdbc/JenisDAO.java
@@ -8,31 +8,28 @@ import com.artivisi.absensi.domain.Jenis;
 import com.artivisi.absensi.domain.Kehadiran;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author bebenpiko
  */
+@Repository
 public class JenisDAO {
-    private final Connection connection;
-    public JenisDAO() throws Exception {
-        // inisialisasi driver database
-        Class.forName("org.postgresql.Driver").newInstance();
-        
-        String url = "jdbc:postgresql://localhost/absensi";
-        String username = "absensi";
-        String password = "absensi";
-        
-        connection = DriverManager.getConnection(url, username, password);
-    }
+    @Autowired private DataSource dataSource;
     
     public List<Jenis> cariSemuaJenis() throws Exception {
         String sql = "select * from jenis";
         List<Jenis> hasil = new ArrayList<Jenis>();
+        Connection connection=dataSource.getConnection();
         ResultSet rs = connection.createStatement().executeQuery(sql);
         while(rs.next()){
             Jenis k = new Jenis();
@@ -40,9 +37,11 @@ public class JenisDAO {
             k.setNama(rs.getString("nama"));
             k.setDendaTelat(rs.getDouble("denda_telat"));
             k.setTarifLembur(rs.getDouble("tarif_lembur"));
-            
             hasil.add(k);
         }
         return hasil;
+        
+    
+    
     }
 }
