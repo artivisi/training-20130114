cd55c4e0e88d7dcef14dcac7aec95c8523b954e5
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-16 09:57:34 +0700

    penjelasan akses database

--- aplikasi-absensi/src/main/java/com/artivisi/absensi/dao/jdbc/JamKerjaDao.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/dao/jdbc/JamKerjaDao.java
@@ -28,21 +28,34 @@ public class JamKerjaDao {
         String username = "absensi";
         String password = "absensi";
         
+        // membuat koneksi ke database
         connection = DriverManager.getConnection(url, username, password);
     }
     
     public List<JamKerja> cariSemuaJamKerja() throws Exception {
         String sql = "select * from JamKerja";
         List<JamKerja> hasil = new ArrayList<JamKerja>();
+        
+        // koneksi digunakan untuk menjalankan query
+        // hasilnya berupa object ResultSet
         ResultSet rs = connection.createStatement().executeQuery(sql);
-        while(rs.next()){
+        while(rs.next()){ // kalau masih ada datanya, rs.next -> true
+            // hasil query dibungkus dalam object JamKerja
             JamKerja k = new JamKerja();
-            k.setKode(rs.getString("kode"));
+            
+            // setelah di next, rs akan berisi row data saat ini
+            k.setKode(rs.getString("kode")); // ambil data untuk kolom kode (tipe data String/varchar)
             k.setNama(rs.getString("nama"));
+            
+            // ambil data untuk kolom mulai (tipe data Timestamp)
             k.setMulai(new Time(rs.getTime("mulai").getTime()));
             k.setSelesai(new Time(rs.getTime("selesai").getTime()));
+            
+            // masukkan object JamKerja yang sudah diisi data
             hasil.add(k);
         }
+        
+        // kembalikan list berisi JamKerja hasil query dari database
         return hasil;
     }
 
