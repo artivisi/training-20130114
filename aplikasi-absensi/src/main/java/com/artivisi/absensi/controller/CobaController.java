3d66279d5a8ba850b637a9559f866cfa3074c852
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-17 17:41:58 +0700

    konfigurasi view resolver supaya bisa menggunakan template jsp

--- aplikasi-absensi/src/main/java/com/artivisi/absensi/controller/CobaController.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/controller/CobaController.java
@@ -4,7 +4,9 @@
  */
 package com.artivisi.absensi.controller;
 
+import java.util.Date;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +21,16 @@ public class CobaController {
     @RequestMapping("/halo")
     @ResponseBody
     public String halo(@RequestParam(required=false) String nama){
-        return "Halo "+nama;
+        return "Halo <b>"+nama+"</b>";
+    }
+    
+    @RequestMapping("/halohtml")
+    public ModelMap haloHtml(@RequestParam(required=false) String nama){
+        ModelMap mm = new ModelMap();
+        
+        mm.addAttribute("namaPengunjung", nama);
+        mm.addAttribute("jamServer", new Date());
+        
+        return mm;
     }
 }
