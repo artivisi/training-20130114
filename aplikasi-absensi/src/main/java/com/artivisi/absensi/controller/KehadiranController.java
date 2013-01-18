76a1f294e41e4f8d1e7bf5752b84ca1ebecd01a8
Endy Muhardin <endy.muhardin@gmail.com>
2013-01-18 14:36:06 +0700

    integrasi jasper report

--- aplikasi-absensi/src/main/java/com/artivisi/absensi/controller/KehadiranController.java
+++ aplikasi-absensi/src/main/java/com/artivisi/absensi/controller/KehadiranController.java
@@ -11,6 +11,7 @@ import com.artivisi.absensi.service.AplikasiAbsenService;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -30,69 +31,87 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class KehadiranController {
+
     @Autowired
     private AplikasiAbsenService service;
-    
+
     @InitBinder
-    public void initBinder(WebDataBinder binder){
-        
+    public void initBinder(WebDataBinder binder) {
+
         // converter untuk tipe data tanggal (Date)
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         binder.registerCustomEditor(Date.class, new CustomDateEditor(formatter, true));
-        
+
         // konverter untuk tipe data Peserta
         PesertaConverter px = new PesertaConverter(service);
         binder.registerCustomEditor(Peserta.class, px);
     }
-    
+
     @RequestMapping("/kehadiran/list")
-    public ModelMap daftarKehadiran(){
+    public ModelMap daftarKehadiran() {
         ModelMap mm = new ModelMap();
-        
+
         List<Kehadiran> data = service.cariSemuaKehadiran();
         mm.addAttribute("daftarKehadiran", data);
-        System.out.println("Jumlah data : "+data.size());
-        
+        System.out.println("Jumlah data : " + data.size());
+
         return mm;
     }
-    
+
     @ModelAttribute("daftarPeserta")
-    public List<Peserta> daftarPeserta(){
+    public List<Peserta> daftarPeserta() {
         return service.cariSemuaPeserta();
     }
-    
-    @RequestMapping(value="/kehadiran/form", method= RequestMethod.GET)
-    public ModelMap tampilkanForm(@RequestParam(required=false) Integer id){
+
+    @RequestMapping(value = "/kehadiran/form", method = RequestMethod.GET)
+    public ModelMap tampilkanForm(@RequestParam(required = false) Integer id) {
         System.out.println("Tampilkan form kehadiran");
         ModelMap mm = new ModelMap();
-        
+
         Kehadiran k = service.cariKehadiranById(id);
-        if(k == null){
+        if (k == null) {
             k = new Kehadiran();
         }
-        
+
         mm.addAttribute("kehadiran", k);
         return mm;
     }
-    
-    @RequestMapping(value="/kehadiran/form", method= RequestMethod.POST)
-    public String prosesForm(@ModelAttribute @Valid Kehadiran x, BindingResult errors){
+
+    @RequestMapping(value = "/kehadiran/form", method = RequestMethod.POST)
+    public String prosesForm(@ModelAttribute @Valid Kehadiran x, BindingResult errors) {
         System.out.println("Memproses form kehadiran");
-        if(x.getPeserta() != null){
-            System.out.println("Peserta : "+x.getPeserta().getNomor());
+        if (x.getPeserta() != null) {
+            System.out.println("Peserta : " + x.getPeserta().getNomor());
         }
-        System.out.println("Jam Masuk : "+x.getJamMasuk());
-        System.out.println("Jam Pulang : "+x.getJamPulang());
-        
-        if(x.getPeserta().getId() == null){
+        System.out.println("Jam Masuk : " + x.getJamMasuk());
+        System.out.println("Jam Pulang : " + x.getJamPulang());
+
+        if (x.getPeserta().getId() == null) {
             errors.rejectValue("peserta", "kehadiran.peserta.invalid");
         }
-        
-        if(errors.hasErrors()) {
+
+        if (errors.hasErrors()) {
             return "/kehadiran/form";
-        } 
-        
+        }
+
         service.simpan(x);
         return "redirect:list";
+    }
+
+    @RequestMapping("/kehadiran/laporan-kehadiran")
+    public ModelMap laporanKehadiran(HttpServletRequest request) {
+        ModelMap mm = new ModelMap();
+
+        Date mulai = new Date();
+        Date sampai = new Date();
+
+        String uri = request.getRequestURI();
+        String format = uri.substring(uri.lastIndexOf(".") + 1);
+
+        mm.addAttribute("mulai", mulai);
+        mm.addAttribute("sampai", sampai);
+        mm.addAttribute("format", format);
+
+        return mm;
     }
 }
